fa53e1dfd011a29b60a2cd97d4e10a63af2a19cb
nthng-quan-wsl <nthquan7603@gmail.com>
2025-04-30 00:46:59 +0700

    wip: init refactor

--- server/app.py
+++ server/app.py
@@ -1,24 +1,21 @@
 import os
-import json
-from utils import *
-from flask import (
-    Flask,
-    jsonify,
-    request,
-    send_from_directory,
-    redirect,
-    url_for
-)
+
+import utils
+from flask import Flask, jsonify, request, send_from_directory, redirect, url_for
+from datetime import datetime
+import requests
+from flask.wrappers import Response
 
 app = Flask(__name__)
 
-cfg = read_file("config.json")
+cfg = utils.read_file("config.json")
 server_cfg = cfg["server"]
-server_url = f"http://{server_cfg['host']}:{server_cfg['port']}"    
+server_url = f"http://{server_cfg['host']}:{server_cfg['port']}"
 node_red_url = f"http://{cfg['node_red']['host']}:{cfg['node_red']['port']}"
 camera_url = f"http://{cfg['esp32_cam']['host']}"
 
-model = Model(model_path=server_cfg['model'], model_name='yolo')
+model = utils.Model(model_path=server_cfg["model"], model_name="yolo")
+
 
 @app.route("/", methods=["GET"])
 def home():
@@ -30,92 +27,96 @@ def status():
     return jsonify({"message": "ok"})
 
 
-@app.route("/config", methods=["GET", "POST"])
-def config_route():
-    config = read_file("config.json")
-    if request.method == "POST":
-        if request:
-            data = request.get_json()
-            config["iot_device"] = data
-            update_file("config.json", config)
-            return jsonify({"message": "Config updated"})
-        else:
-            return jsonify({"message": "No update"})
-    else:
-        if request.user_agent.string.lower() == "esp8266httpclient":
-            return jsonify(config["iot_device"])
-        return config
+@app.route("/config", methods=["GET"])
+def get_config():
+    config = utils.read_file("config.json")
+    if request.user_agent.string.lower() == "esp8266httpclient":
+        return jsonify(config.get("iot_device", {}))
+    return config
 
 
-@app.route("/system", methods=["GET", "POST"])
-def system():
-    if request.method == "POST":
-        if request:
-            now = datetime.now()
-            formatted_time = now.strftime("%Y-%m-%d-%H-%M-%S")
-            data = request.get_json()
-            reponse = jsonify({
-                "time" : formatted_time,
-                "data": data
-            })
-            update_file("data.json", reponse.json)
-            log_data(data, "./log/system.csv")
-            return jsonify({"message": "Upload ok"})
-        else:
-            return jsonify({"message": "Nothing uploaded"})
+@app.route("/config", methods=["POST"])
+def update_config():
+    data = request.get_json()
 
-    if request.method == "GET":
-        data = read_file("data.json")
-        return jsonify(data)
+    config = utils.read_file("config.json")
+    config["iot_device"] = data
+    utils.update_file("config.json", config)
+    return jsonify({"message": "Config updated"})
 
 
-@app.route("/fire", methods=["GET", "POST"])
-def fire():
-    if request.method == "POST":
-        if request.data.decode('utf-8') == "check":
-            print("* Init check fire")
-            return jsonify({"status": "ok"})
-        else:
-            system_data = request.get_json()
-            img_dir, img_url = capture_image()
-            result = model.predict(img_dir)
-            
-            if len(result) > 1: # yolo bboxes
-                img_url = result[1]
-                result = result[0]
+@app.route("/system", methods=["GET"])
+def get_system_data():
+    data = utils.read_file("data.json")
+    return jsonify(data)
 
-            log_data(system_data, "./log/fire.csv", result, img_url)
 
-            if status == -1:
-                return jsonify({"error": "Error capturing image"})
-            else:
-                return jsonify({
-                    "fire": result,
-                    "url": img_url
-                })
-    else:
-        result = get_fire_report("./log/fire.csv")
-        return result
+@app.route("/system", methods=["POST"])
+def post_system_data():
+    data = request.get_json()
+    if not data:
+        return jsonify({"message": "Nothing uploaded"}), 400
+
+    formatted_time = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    response_data = {"time": formatted_time, "data": data}
+
+    utils.update_file("data.json", response_data)
+    utils.log_data(data, "./log/system.csv")
+
+    return jsonify({"message": "Upload ok"})
+
+
+@app.route("/fire", methods=["GET"])
+def get_fire_report():
+    return utils.get_fire_report("./log/fire.csv")
+
+
+@app.route("/fire", methods=["POST"])
+def check_or_detect_fire():
+    # Handle ESP32 "check"
+    if request.data.decode("utf-8") == "check":
+        print("* Init check fire")
+        return jsonify({"status": "ok"})
+
+    # Handle actual fire detection
+    system_data = request.get_json()
+    img_dir, img_url = utils.capture_image()
+
+    if img_dir == -1:
+        return jsonify({"error": "Error capturing image"}), 500
+
+    result = model.predict(img_dir)
+
+    if len(result) > 1:
+        img_url = result[1]
+        result = result[0]
+
+    utils.log_data(system_data, "./log/fire.csv", result, img_url)
+
+    return jsonify({"fire": result, "url": img_url})
 
 
 @app.route("/image/<path:filename>", methods=["GET"])
 def get_image(filename):
-    img_path_plain = os.path.join(f"{server_cfg['image_dir']}/plain", filename)
-    img_path_yolo = os.path.join(f"{server_cfg['image_dir']}/yolo_output", filename)
+    base_img_dir = server_cfg["image_dir"]
+    paths = {
+        "plain": os.path.join(base_img_dir, "plain", filename),
+        "yolo": os.path.join(base_img_dir, "yolo_output", filename),
+    }
 
-    if not os.path.isfile(img_path_plain) and not os.path.isfile(img_path_yolo):
-        return jsonify({"error": f"Image {filename} not found"})
-    else:
-        try:
-            return send_from_directory(\
-                f"{server_cfg['image_dir']}/plain", filename, as_attachment=True)
-        except:
-            return send_from_directory(\
-                f"{server_cfg['image_dir']}/yolo_output", filename, as_attachment=True)
+    for subdir, path in paths.items():
+        if os.path.isfile(path):
+            image: Response = send_from_directory(
+                os.path.join(base_img_dir, subdir), filename, as_attachment=True
+            )
+            return image
+
+    return jsonify({"error": f"Image {filename} not found"}), 404
+
 
 @app.route("/capture", methods=["GET"])
 def capture():
-    img_dir, img_url = capture_image()
+    img_dir, img_url = utils.capture_image()
     if request.user_agent.string.lower() == "esp8266httpclient":
         requests.post(f"{node_red_url}/capture", json={"img_url": img_url})
         return jsonify({"success": "Forwarded to node-red"})
@@ -126,25 +127,25 @@ def capture():
         return jsonify({"img_url": img_url})
 
 
-@app.route("/config/camera", methods=["GET", "POST"])
-def camera_config():
-    if request.method == "POST":
-        if request:
-            data = request.get_json()
-            result = set_camera_parameters(data)
+@app.route("/config/camera", methods=["GET"])
+def get_camera_config():
+    return redirect(url_for("routes.config_route"))
 
-            if result == 1:
-                update_camera_cfg() # get /status -> update config.json
-                return jsonify({"message": "Config updated"})
-            else:
-                return jsonify({"message": result})
 
-        else:
-            return jsonify({"message": "No update"})
+@app.route("/config/camera", methods=["POST"])
+def update_camera_config():
+    data = request.get_json()
+    if not data:
+        return jsonify({"message": "No update"}), 400
 
+    result = utils.set_camera_parameters(data)
+
+    if result == 1:
+        utils.update_camera_cfg()  # sync config.json
+        return jsonify({"message": "Config updated"})
     else:
-        return redirect(url_for("config_route"))
+        return jsonify({"message": result})
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5555)
+    app.run(debug=True, host="0.0.0.0", port=5555)
--- server/utils/model.py
+++ server/utils/model.py
@@ -1,93 +1,89 @@
-from PIL import Image, ImageDraw
-import numpy as np
-
-import torch
-import mlflow.pytorch
-import torch.nn as nn
-import torchvision.transforms as transforms
-import torchvision.models as models
-
-from ultralytics import YOLO
-from PIL import Image
-
 import json
+
+import mlflow.pytorch
+import numpy as np
+import torch
+import torch.nn as nn
+import torchvision.models as models
+import torchvision.transforms as transforms
+from PIL import Image, ImageDraw
+from ultralytics import YOLO
+
 from .helper import read_file, update_file
 
 mlflow.set_tracking_uri("http://linux:5000")
 mlflow.set_experiment("fire-detection")
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-data_transform = transforms.Compose([
-    transforms.Resize((224, 224)),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], 
-                        std=[0.229, 0.224, 0.225])
-])
+data_transform = transforms.Compose(
+    [
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ]
+)
 
 cfg = read_file("config.json")
 server_cfg = cfg["server"]
-server_url = f"http://{server_cfg['host']}:{server_cfg['port']}"    
+server_url = f"http://{server_cfg['host']}:{server_cfg['port']}"
 node_red_url = f"http://{cfg['node_red']['host']}:{cfg['node_red']['port']}"
 camera_url = f"http://{cfg['esp32_cam']['host']}"
 
+
 class Model(object):
-    def __init__(self, num_classes=2, model_path="./model", model_name='yolo'):
-        if model_name == 'resnet':
+    def __init__(self, num_classes=2, model_path="./model", model_name="yolo"):
+        if model_name == "resnet":
             try:
                 print(f"-> Reloading model from {model_path}...")
                 self.model = ReloadModel(model_path)
-            except:
-                print(f"-> Reloading fail, using default Resnet18")
-                self.model = Resnet18(num_classes, freeze=True)
+            except Exception as e:
+                print(f"-> {e} Reloading fail, using default Resnet18")
+                self.model = Resnet18(num_classes)
             self.model.eval()
-        elif model_name == 'yolo':
-            self.model = YoloV8(model_path)
 
+        elif model_name == "yolo":
+            self.model = YoloV8(model_path)
 
     def predict(self, img_fn):
         print(img_fn)
         return self.model.predict(img_fn)
 
+
 class ReloadModel(nn.Module):
     def __init__(self, model_path) -> None:
         super().__init__()
-        if torch.cuda.is_available():
-            self.model = mlflow.pytorch.load_model(model_path)
-        else:
-            self.model = mlflow.pytorch.load_model(
-                model_path, map_location=torch.device('cpu'))
-
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.model = mlflow.pytorch.load_model(
+            model_path, map_location=torch.device(device)
+        )
         self.model.eval()
 
     def predict(self, img_fn):
         with torch.inference_mode():
-            img = Image.open(img_fn).convert('RGB')
+            img = Image.open(img_fn).convert("RGB")
             img = data_transform(img)
             img = img.to(device)
 
             result = self.model(img.unsqueeze(0))
             result = torch.argmax(result, dim=1)
-        
+
         return result.item()
 
+
 class Resnet18(nn.Module):
-    def __init__(self, num_classes, freeze=True):
+    def __init__(self, num_classes):
         super().__init__()
         self.model = models.resnet18()
-        if freeze:
-            for param in self.model.parameters():
-                param.require_grad = False
-        
         self.model.fc = nn.Linear(512, num_classes)
         self.model.to(device)
         self.model.eval()
-    
+
     def forward(self, x):
         return self.model(x)
 
     def predict(self, img_fn):
         with torch.inference_mode():
-            img = Image.open(img_fn).convert('RGB')
+            img = Image.open(img_fn).convert("RGB")
             img = data_transform(img)
             img = img.to(device)
 
@@ -96,7 +92,8 @@ class Resnet18(nn.Module):
 
         return result.item()
 
-class YoloV8():
+
+class YoloV8:
     def __init__(self, model_path="./model/yolo"):
         super().__init__()
         self.model = YOLO(model_path)
@@ -115,10 +112,9 @@ class YoloV8():
         config = read_file("config.json")
         img_dir = f"{config['server']['image_dir']}/yolo_output"
         output_path = f"{img_dir}/{img_fn.split('/')[-1].split('.')[0]}_yolo.jpg"
-        
-        print('-> Saving yolo output to', output_path)
+
+        print("-> Saving yolo output to", output_path)
         img_url = f"{server_url}/image/{output_path.split('/')[-1]}"
         image.save(output_path)
 
         return int(len(results[0].boxes.cls) > 0), img_url
-        
